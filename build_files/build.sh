38bcb1f402a2b393bc88ddb18766213abb55d32b
MechaRavenME <mecharaven1997@gmail.com>
2026-03-25 20:25:52 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,19 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+    tmux \
+    kvantum \
+    R \
+    hyprland \
+    waybar \
+    wofi \
+    mako \
+    hyprlock \
+    hyprpaper \
+    grim \
+    slurp \
+    xdg-desktop-portal-hyprland
 
 # Use a COPR Example:
 #
