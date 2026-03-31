fad6d24c5b5f0327882a1bc5ea5e6c1e500b567a
MechaRavenME <mecharaven1997@gmail.com>
2026-03-30 23:36:24 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,11 +8,11 @@ set -ouex pipefail
 dnf5 install -y kvantum R 
 dnf5 copr enable -y iucar/rstudio && dnf5 install -y rstudio-desktop
 
-# Habilitar repositorios COPR actualizados (Nativos para Fedora 43)
+# Habilitar repositorio COPR principal
 dnf5 copr enable -y solopasha/hyprland
-dnf5 copr enable -y retrozinndev/ags
 
-# 1. Instalar Hyprland y dependencias (EXCEPTO ags)
+# Instalar Hyprland y dependencias
+# Agregamos explícitamente aylurs-gtk-shell (el nombre real del paquete en el COPR, en lugar de solo 'ags' que choca con el juego)
 dnf5 install -y \
   hyprland xdg-desktop-portal-hyprland \
   hyprpaper hyprlock hypridle hyprpicker \
@@ -21,10 +21,9 @@ dnf5 install -y \
   swww \
   kitty sassc fd-find ripgrep \
   brightnessctl playerctl \
-  unzip wget cargo
-
-# 2. Instalar AGS obligando a DNF a usar el COPR (Ignorando el motor de videojuegos de Fedora)
-dnf5 install -y ags --disablerepo=fedora --disablerepo=updates --disablerepo=updates-archive
+  unzip wget cargo \
+  aylurs-gtk-shell \
+  gjs 
 
 # --- Instalar Bun (Método de descarga directa) ---
 wget https://github.com/oven-sh/bun/releases/latest/download/bun-linux-x64.zip -O bun.zip
@@ -33,7 +32,6 @@ mv bun-linux-x64/bun /usr/bin/
 rm -rf bun-linux-x64 bun.zip
 
 # --- Instalar Matugen (Compilación segura con Cargo) ---
-# Usamos un directorio temporal en /tmp para evitar problemas de permisos de OSTree
 env CARGO_HOME=/tmp/cargo cargo install matugen
 mv /tmp/cargo/bin/matugen /usr/bin/
 rm -rf /tmp/cargo
@@ -42,7 +40,6 @@ rm -rf /tmp/cargo
 dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr disable iucar/rstudio
 dnf5 -y copr disable solopasha/hyprland
-dnf5 -y copr disable retrozinndev/ags
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
