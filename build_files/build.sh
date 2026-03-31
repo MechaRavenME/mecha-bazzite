a9f98d0854a6624fa70e5df71baab596dfe36087
MechaRavenME <mecharaven1997@gmail.com>
2026-03-31 00:19:03 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,41 +4,31 @@ set -ouex pipefail
 
 ### Install packages
 
-# R y RStudio
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
 dnf5 install -y kvantum R 
 dnf5 copr enable -y iucar/rstudio && dnf5 install -y rstudio-desktop
 
-# Habilitar repositorio COPR principal
-dnf5 copr enable -y solopasha/hyprland
-
-# Instalar Hyprland y dependencias
+# Habilitar el repositorio de Hyprland y luego instalar el WM y todas las utilidades
+dnf5 copr enable -y solopasha/hyprland && \
 dnf5 install -y \
   hyprland xdg-desktop-portal-hyprland \
   hyprpaper hyprlock hypridle hyprpicker \
   waybar rofi-wayland swaync wlogout \
-  grim slurp pavucontrol \
-  swww \
-  kitty sassc fd-find ripgrep \
-  brightnessctl playerctl \
-  unzip wget cargo \
-  aylurs-gtk-shell \
-  gjs gobject-introspection gtk-layer-shell libdbusmenu-gtk3 libgtop2
+  grim slurp wl-clipboard \
+  pavucontrol
 
-# --- Instalar Bun (Método de descarga directa) ---
-wget https://github.com/oven-sh/bun/releases/latest/download/bun-linux-x64.zip -O bun.zip
-unzip bun.zip
-mv bun-linux-x64/bun /usr/bin/
-rm -rf bun-linux-x64 bun.zip
-
-# --- Instalar Matugen (Compilación segura con Cargo) ---
-env CARGO_HOME=/tmp/cargo cargo install matugen
-mv /tmp/cargo/bin/matugen /usr/bin/
-rm -rf /tmp/cargo
-
-# Disable COPRs so they don't end up enabled on the final image
+# Use a COPR Example:
+#
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable ublue-os/staging
-dnf5 -y copr disable iucar/rstudio
-dnf5 -y copr disable solopasha/hyprland
 
 #### Example for enabling a System Unit File
+
 systemctl enable podman.socket
