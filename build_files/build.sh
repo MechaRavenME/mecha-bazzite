0df860f86578bd6e4a1786acc10f8081a54010cf
MechaRavenME <mecharaven1997@gmail.com>
2026-03-30 21:44:33 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,33 +4,39 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
+# R y RStudio
 dnf5 install -y kvantum R 
 dnf5 copr enable -y iucar/rstudio && dnf5 install -y rstudio-desktop
 
-# Habilitar el repositorio de Hyprland y luego instalar el WM y todas las utilidades
-dnf5 copr enable -y solopasha/hyprland && \
+# Habilitar repositorios COPR para Hyprland, AGS y swww
+dnf5 copr enable -y solopasha/hyprland
+dnf5 copr enable -y aylur/ags
+dnf5 copr enable -y tofik/swww
+
+# Instalar Hyprland, utilidades base y las dependencias de Caelestia Shell
 dnf5 install -y \
   hyprland xdg-desktop-portal-hyprland \
   hyprpaper hyprlock hypridle hyprpicker \
   waybar rofi-wayland swaync wlogout \
   grim slurp wl-clipboard \
   pavucontrol \
-  brightnessctl playerctl dart-sass fd-find ripgrep btop \
-  varlad/ags tofik/swww cargo
+  ags swww \
+  kitty dart-sass fd-find ripgrep btop \
+  brightnessctl playerctl ImageMagick \
+  nodejs npm cargo
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+# Instalar dependencias externas (Bun y Matugen)
+# Caelestia usa Bun como entorno de ejecución y Matugen para generar colores dinámicos
+npm install -g bun
+cargo install matugen --root /usr
+
 # Disable COPRs so they don't end up enabled on the final image:
+# Es vital deshabilitarlos al final para evitar problemas con rpm-ostree en el futuro
 dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable iucar/rstudio
+dnf5 -y copr disable solopasha/hyprland
+dnf5 -y copr disable aylur/ags
+dnf5 -y copr disable tofik/swww
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
