e2c1bc07ed1bef3bf97d36af2c537affeaeef48b
MechaRavenME <mecharaven1997@gmail.com>
2026-03-30 22:26:09 -0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,28 +9,27 @@ dnf5 install -y kvantum R
 dnf5 copr enable -y iucar/rstudio && dnf5 install -y rstudio-desktop
 
 # Habilitar repositorios COPR actualizados (Nativos para Fedora 43)
-# Solopasha ya incluye paquetes extra como swww además de Hyprland
 dnf5 copr enable -y solopasha/hyprland
-# Retrozinndev mantiene AGS compilado para Fedora 43
 dnf5 copr enable -y retrozinndev/ags
 
-# Instalar Hyprland, utilidades base y las dependencias de Caelestia Shell
+# Instalar Hyprland y dependencias
+# HE ELIMINADO: wl-clipboard, btop, ImageMagick y nodejs porque Bazzite ya los trae.
+# HE CAMBIADO: dart-sass por sassc.
 dnf5 install -y \
   hyprland xdg-desktop-portal-hyprland \
   hyprpaper hyprlock hypridle hyprpicker \
   waybar rofi-wayland swaync wlogout \
-  grim slurp wl-clipboard \
-  pavucontrol \
+  grim slurp pavucontrol \
   ags swww \
-  kitty dart-sass fd-find ripgrep btop \
-  brightnessctl playerctl ImageMagick \
-  nodejs npm cargo
+  kitty sassc fd-find ripgrep \
+  brightnessctl playerctl \
+  npm cargo
 
 # Instalar dependencias externas (Bun y Matugen)
 npm install -g bun
 cargo install matugen --root /usr
 
-# Disable COPRs so they don't end up enabled on the final image:
+# Disable COPRs so they don't end up enabled on the final image
 dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr disable iucar/rstudio
 dnf5 -y copr disable solopasha/hyprland
